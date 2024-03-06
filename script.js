ac212989ec2c800ee92a5f6d3fd2fb0b28b37cc1
n-whittaker <64340124+n-whittaker@users.noreply.github.com>
2024-03-06 13:03:25 +0000

    Add comments

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function divide(a, b) {
     return a / b
 }
 
-function operate(a, op, b) {
+function operate(a, op, b) {  // Selects the required operation.
     if (op === "+") {
         return add(a, b);
     } else if (op === "-") {
@@ -37,14 +37,14 @@ function operate(a, op, b) {
     }
 }
 
-function storeFirstNum() {
+function storeFirstNum() { // Stores whatever is in the display in num1
     num1 = parseInt(displayValue.textContent)
 }
 
-function storeSecondNum() {
+function storeSecondNum() { // Stores whatever is in the display in num2
     num2 = parseInt(displayValue.textContent)
 }
-function populateDisplay(val) {
+function populateDisplay(val) {  // Adds a value to the display
     displayValue.textContent += val;
 }
 
@@ -53,9 +53,9 @@ function clearDisplay() {
 }
 
 for (const btn of numButtons) {
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', () => { // Event listener for the Number buttons
 
-        if (num1 !== undefined) {
+        if (num1 !== undefined) { // Only clear the display if the num1 is filled.
             clearDisplay()
         }
 
@@ -64,35 +64,35 @@ for (const btn of numButtons) {
     })
 }
 
-clearBtn.addEventListener('click', () => {
+clearBtn.addEventListener('click', () => { // Clears display and resets num1
     clearDisplay()
     num1 = undefined;
 })
 
-equalsBtn.addEventListener('click', () => {
+equalsBtn.addEventListener('click', () => { // Performs calculation
     calculate();
-
 })
 
 
-for (const btn of opButtons) {
+for (const btn of opButtons) {      //Operator buttons
     btn.addEventListener('click', () => {
-        if (num1 === undefined) {
+        if (num1 === undefined) {       // Store display value in num1 if empty, if full, calculate.
             storeFirstNum();
         } else {
             calculate()
         }
 
-        operator = btn.textContent;
+        operator = btn.textContent;     // Set the operator to the operator button the user clicked.
 
-        num1 = parseInt(displayValue.textContent);
+        num1 = parseInt(displayValue.textContent);  // This allows for sums of any length like 3 + 2 * 4 / 9
+                                                    // without having to press "-" every time
 
     })
 }
 
 function calculate() {
-    storeSecondNum()
-    clearDisplay();
+    storeSecondNum()        // Store the display content as the second number, clear the display and then perform the
+    clearDisplay();         // calculation, population display with the result.
     let result = operate(num1, operator, num2);
     populateDisplay(result);
 
