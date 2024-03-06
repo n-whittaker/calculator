d3743ec496b21f953abc3a6511272afc6065180f
n-whittaker <64340124+n-whittaker@users.noreply.github.com>
2024-03-06 16:34:35 +0000

    Add decimal functionality

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const numButtons = document.querySelectorAll(".btn.number")
 const opButtons = document.querySelectorAll(".btn.operator")
 const clearBtn = document.querySelector(".clear");
 const equalsBtn = document.querySelector(".equals");
-const displayValue = document.querySelector(".display-text")
+const displayValue = document.querySelector(".display-text");
+let newNum = true;
+
 
 
 function add(a, b) {
@@ -52,15 +54,15 @@ function clearDisplay() {
     displayValue.textContent = "";
 }
 
-for (const btn of numButtons) {
-    btn.addEventListener('click', () => { // Event listener for the Number buttons
+for (const btn of numButtons) { // Number buttons
+    btn.addEventListener('click', () => {
 
-        if (num1 !== undefined) { // Only clear the display if the num1 is filled.
+        if (newNum) {
             clearDisplay()
         }
 
         displayValue.textContent += btn.textContent; // Update display
-
+        newNum = false; // Next number will not be a new number
     })
 }
 
@@ -83,7 +85,8 @@ for (const btn of opButtons) {      //Operator buttons
         }
 
         operator = btn.textContent;     // Set the operator to the operator button the user clicked.
-
+        newNum = true;                  // tells program that the next number will be a number, different number than
+                                        //  previously inputted.
         num1 = parseInt(displayValue.textContent);  // This allows for sums of any length like 3 + 2 * 4 / 9
                                                     // without having to press "-" every time
 
@@ -94,6 +97,27 @@ function calculate() {
     storeSecondNum()        // Store the display content as the second number, clear the display and then perform the
     clearDisplay();         // calculation, population display with the result.
     let result = operate(num1, operator, num2);
-    populateDisplay(result);
+    let decimalsCount;
+
+
+    // The following changes 1.5340000 into 1.534
+    if (result === Math.round(result)) { // If integer
+        populateDisplay(result);
+    } else {    // If floating point
+        let decimals = (Math.round(result) - result);
+
+
+        if (decimals < 0) {
+            decimalsCount = (decimals.toString().length) - 3;// -3 to account for the "-0." in "-0.535"
+        } else {
+            decimalsCount = (decimals.toString().length) - 2 ; // -2 to account for the "0." in "0.535"
+        }
+
+        // NEED TO CAP AMOUNT OF DECIMALS
+
+        populateDisplay(result.toFixed(decimalsCount))
+        console.log(result, decimals, decimalsCount);
+    }
+
 
 }
